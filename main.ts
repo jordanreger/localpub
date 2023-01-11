bc2ca0de1ffde1c99283dfbeb58bf2dce9d93563
Jordan Reger <mail@jordanreger.com>
2023-01-10 21:28:45 -0500

    feat: revamp auth

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 // deno-lint-ignore-file
-import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 import "https://deno.land/std@0.170.0/dotenv/load.ts";
+
+import { serve, getCookies } from "https://deno.land/std@0.171.0/http/mod.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.2.3";
 
 const supabase_url = Deno.env.get("SUPABASE_URL"), supabase_key = Deno.env.get("SUPABASE_KEY"), supabase = createClient(supabase_url!, supabase_key!);
@@ -8,23 +9,97 @@ const supabase_url = Deno.env.get("SUPABASE_URL"), supabase_key = Deno.env.get("
 async function handler(req: Request): Promise<Response> {
   const url = new URL(req.url);
   const path = url.pathname;
+  const origin = url.origin;
   const file = (fp:string) => { return Deno.readFile(fp) };
   const route = (route:string) => { const regexRoute = new RegExp(route, "gmi"); if(regexRoute.test(path)){ return path } else { return null }}
 
+  const access_token = getCookies(req.headers)["access-token"], refresh_token = getCookies(req.headers)["refresh-token"];
+
   if(path === "/") {
-    return new Response(await file("./src/index.html"), { headers: { "content-type": "text/html; charset=utf-8" } });
+    if(access_token && refresh_token) {
+      await supabase.auth.setSession({
+        refresh_token: refresh_token,
+        access_token: access_token
+      })
+      return Response.redirect(`${origin}/app`, 302);
+    } else {
+      return new Response(await file("./src/index.html"), { headers: { "content-type": "text/html; charset=utf-8" } });
+    }
   }
 
   else if(path === "/register") {
-    return new Response(await file("./src/register.html"), { headers: { "content-type": "text/html; charset=utf-8" } });
+    if(access_token && refresh_token) {
+      await supabase.auth.setSession({
+        refresh_token: refresh_token,
+        access_token: access_token
+      })
+      return Response.redirect(`${origin}/app`, 302);
+    } else {
+      return new Response(await file("./src/register.html"), { headers: { "content-type": "text/html; charset=utf-8" } });
+    }
+  }
+
+  else if(path === "/register-auth") {
+    const auth = JSON.parse(await req.text()), username = auth.username, email = auth.email, password = auth.password;
+    const register = supabase.auth.signUp({
+      email: email,
+      password: password,
+      options: {
+        data: {
+          username: username
+        }
+      }
+    });
+    const { data, error } = await register;
+    if(!error) {
+      const register_auth_headers = new Headers();
+      register_auth_headers.append('Set-Cookie', `access-token=${data.session?.access_token}; path=/; max-age=3153600000; SameSite=Lax; secure`);
+      register_auth_headers.append('Set-Cookie', `refresh-token=${data.session?.refresh_token}; path=/; max-age=3153600000; SameSite=Lax; secure`);
+      return new Response("registered", { headers: register_auth_headers });
+    } else {
+      return new Response(String(error));
+    }
   }
 
   else if(path === "/login") {
-    return new Response(await file("./src/login.html"), { headers: { "content-type": "text/html; charset=utf-8" } });
+    if(access_token && refresh_token) {
+      await supabase.auth.setSession({
+        refresh_token: refresh_token,
+        access_token: access_token
+      })
+      return Response.redirect(`${origin}/app`, 302);
+    } else {
+      return new Response(await file("./src/login.html"), { headers: { "content-type": "text/html; charset=utf-8" } });
+    }
+  }
+
+  else if(path === "/login-auth") {
+    const auth = JSON.parse(await req.text()), email = auth.email, password = auth.password;
+    const login = supabase.auth.signInWithPassword({
+      email: email,
+      password: password
+    });
+    const { data, error } = await login;
+    if(!error) {
+      const login_auth_headers = new Headers();
+      login_auth_headers.append('Set-Cookie', `access-token=${data.session?.access_token}; path=/; max-age=3153600000; SameSite=Lax; secure`);
+      login_auth_headers.append('Set-Cookie', `refresh-token=${data.session?.refresh_token}; path=/; max-age=3153600000; SameSite=Lax; secure`);
+      return new Response("logged in", { headers: login_auth_headers });
+    } else {
+      return new Response(String(error));
+    }
   }
 
   else if(path === "/app") {
-    return new Response(await file("./src/app.html"), { headers: { "content-type": "text/html; charset=utf-8" } });
+    if(access_token && refresh_token) {
+      await supabase.auth.setSession({
+        refresh_token: refresh_token,
+        access_token: access_token
+      })
+      return new Response(await file("./src/app.html"), { headers: { "content-type": "text/html; charset=utf-8" } });
+    } else {
+      return Response.redirect(`${origin}`, 302);
+    }
   }
 
   else if(path === "/feed") {
@@ -33,6 +108,14 @@ async function handler(req: Request): Promise<Response> {
 
   else if(path === "/settings") {
     return new Response(await file("./src/settings.html"), { headers: { "content-type": "text/html; charset=utf-8" } });
+  }
+
+  else if(path === "/logout") {
+    const expires = new Date(0).toUTCString();
+    const logout_headers = new Headers();
+    logout_headers.append('Set-Cookie', `access-token=; path=/; expires=${expires}; SameSite=Lax; secure`);
+    logout_headers.append('Set-Cookie', `refresh-token=; path=/; expires=${expires}; SameSite=Lax; secure`);
+    return new Response("logout", { headers: logout_headers });
   }
 
   else if(path === "/index.css") {
@@ -152,5 +235,4 @@ async function handler(req: Request): Promise<Response> {
     return new Response("not found", { status: 404 });
   }
 }
-console.log("Listening on http://localhost:8000");
 serve(handler);
